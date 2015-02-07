4f18e29ba31286d7c6ac80adc053a0f2a44cb477
glcoakley <itcs2825_kozlevcar@lakeland-community-colleges-imac-9.local>
2015-02-06 19:17:04 -0500

    HR8Lab

---
+++ ITCS2825_Lab4_Coakley/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ITCS2825_Lab4_Coakley
+//
+//  Created by ITCS2825 Kozlevcar on 2/6/15.
+//  Copyright (c) 2015 lcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ITCS2825_Lab4_Coakley/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ITCS2825_Lab4_Coakley
+//
+//  Created by ITCS2825 Kozlevcar on 2/6/15.
+//  Copyright (c) 2015 lcc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
